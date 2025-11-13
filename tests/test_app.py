7ee441e79c24200cc3b06373889b12ac2f23f07a
e <mha119@mail.aub.edu>
2025-11-13 23:50:59 +0200

    Fix import path and unit test for greet

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,20 @@
+import os
+import sys
 import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from app import greet
+
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from Mohammad Aboulhoda!")
+        self.assertEqual(
+            greet("World"),
+            "Hello, World from Mohammad Aboulhoda:",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
+
